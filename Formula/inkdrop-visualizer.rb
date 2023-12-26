c48155518974b76a47acff6fbea1b791a188b92b
albschi <albertoschillaci30@gmail.com>
2023-12-26 17:58:14 +0100

    new package url inkdrop

--- Formula/inkdrop-visualizer.rb
+++ Formula/inkdrop-visualizer.rb
@@ -1,7 +1,7 @@
 class InkdropVisualizer < Formula
   desc "Visualize your Terraform configuration and plan as a graph"
   url "https://github.com/inkdrop-org/terraform-visualizer-cli/releases/download/v1.0.6/inkdrop.tar.gz"
-  sha256 "a100a87d74d9da681472f96230c9929aff3bd1130338a4419907bc87597f1cd2"
+  sha256 "e2a82235e3a3f2fb060b78a8552d18f202178e49713cc338e88d136be46789c5"
   license "MIT"
   version "1.0.6"
 
