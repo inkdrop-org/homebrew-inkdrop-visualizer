70d0b0abd03c60c5eb2b9f697da8421d2c78409a
albschi <86234287+albschi@users.noreply.github.com>
2024-01-04 12:37:14 +0100

    Update inkdrop-visualizer.rb

--- Formula/inkdrop-visualizer.rb
+++ Formula/inkdrop-visualizer.rb
@@ -3,7 +3,7 @@ class InkdropVisualizer < Formula
   url "https://github.com/inkdrop-org/terraform-visualizer-cli/releases/download/v1.0.8/inkdrop-brew-v1.0.8.tar.gz"
   sha256 "6116a5531fac041b119585814d7806f30a3ae9cfe31da543e4d833ad712cf01f"
   license "MIT"
-  version "1.0.6"
+  version "1.0.8"
 
   def install
     bin.install "inkdrop"
