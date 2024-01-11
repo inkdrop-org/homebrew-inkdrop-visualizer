27f11eb9431439b2d1e740bb7a41e4f948992185
albschi <86234287+albschi@users.noreply.github.com>
2024-01-11 19:11:34 +0100

    Update inkdrop-visualizer.rb

--- Formula/inkdrop-visualizer.rb
+++ Formula/inkdrop-visualizer.rb
@@ -5,14 +5,7 @@ class InkdropVisualizer < Formula
   license "MIT"
   version "1.0.16"
 
-  depends_on "node" # Ensure Node.js is installed as playwright requires it
-
   def install
     bin.install "inkdrop"
-  end
-
-  def post_install
-    # This assumes that `npx` is available and in PATH.
-    system "npx", "playwright", "install", "chromium"
   end
 end
