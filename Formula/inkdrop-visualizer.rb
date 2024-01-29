c446c89a557042171733ffed05fe61d53fef11d0
GitHub Action <action@github.com>
2024-01-29 17:54:56 +0000

    Update inkdrop-visualizer to v1.1.8

--- Formula/inkdrop-visualizer.rb
+++ Formula/inkdrop-visualizer.rb
@@ -1,9 +1,9 @@
 class InkdropVisualizer < Formula
   desc "Visualize your Terraform configuration and plan as a graph"
-  url "https://github.com/inkdrop-org/terraform-visualizer-cli/releases/download/v1.1.7/inkdrop-brew-v1.1.7.tar.gz"
-  sha256 "0626750d378d0dffa6454d8298946c4684de040f653d0e203eb54f614fa0f60e"
+  url "https://github.com/inkdrop-org/terraform-visualizer-cli/releases/download/v1.1.8/inkdrop-brew-v1.1.8.tar.gz"
+  sha256 "86fb22775db685306015d33ff57aafaf935b99e559649c9bbf64cc5c324c1819"
   license "MIT"
-  version "1.1.7"
+  version "1.1.8"
 
   def install
     bin.install "inkdrop"
